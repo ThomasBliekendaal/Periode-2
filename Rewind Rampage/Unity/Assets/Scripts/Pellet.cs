c659df433a5d6c3c337ab097bedeab1c37258d9e
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-01-23 16:09:51 +0100

    Models en scripts

---
+++ Rewind Rampage/Unity/Assets/Scripts/Pellet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour {
+    public int damage;
+    public GameObject bloedParticle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            GameObject b = Instantiate(bloedParticle, collision.contacts[0].point, transform.rotation);
+            collision.transform.parent.gameObject.GetComponent<MyHp>().DeathVoid(damage);
+        }
+        if (collision.transform.tag == "EnemyHead")
+        {
+            collision.transform.parent.gameObject.GetComponent<MyHp>().DeathVoid(damage * 2);
+        }
+        if (collision.transform)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        Destroy(gameObject, 10f);
+    }
+}
---
+++ Rewind Rampage/Unity/Assets/Scripts/ShotgunScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunScript : MonoBehaviour {
+    public int pelletCount;
+    public float spreadAngle;
+    public float pelletFireVel=1;
+    public GameObject pellet;
+    public Transform barrel;
+    List<Quaternion> pellets;
+
+	void Awake () {
+        pellets = new List<Quaternion>(pelletCount);
+        for (int i = 0; i < pelletCount; i++)
+        {
+            pellets.Add(Quaternion.Euler(Vector3.zero));
+        }
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fire();
+        }
+	}
+    void fire()
+    {
+        int i = 0;
+        foreach(Quaternion quat in pellets)
+        {
+            pellets[i] = Random.rotation;
+            GameObject p = Instantiate(pellet, barrel.position, barrel.rotation);
+            p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+            p.GetComponent<Rigidbody>().AddForce(p.transform.forward * pelletFireVel);
+            i++;
+        }
+    }
+}
