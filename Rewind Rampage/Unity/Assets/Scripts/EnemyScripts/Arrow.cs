919e17a56665f8ba831eb1c20fc6cb0c16b9ed92
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-01-26 13:11:35 +0100

    bowman refinement

--- Rewind Rampage/Unity/Assets/Scripts/EnemyScripts/Arrow.cs
+++ Rewind Rampage/Unity/Assets/Scripts/EnemyScripts/Arrow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour {
     public int damage;
+    public float despawn;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,17 +12,23 @@ public class Arrow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        despawn -= Time.deltaTime;
+        if (despawn < 0)
+        {
+            Destroy(gameObject);
+        }
 	}
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform)
         {
-            Destroy(gameObject);
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<SphereCollider>().isTrigger = true;
         }
         if (collision.transform.tag == "Player")
         {
             collision.transform.gameObject.GetComponent<PlayerHp>().DeathVoid(damage);
+            Destroy(gameObject);
         }
     }
 }
